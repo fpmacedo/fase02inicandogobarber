6031855bea2804be1839d529662e43086f8b1fe6
fpmacedo <lipe_macedo@msn.com>
2019-10-17 22:50:36 -0300

    Terminando aula de eslint

--- src/app.js
+++ src/app.js
@@ -1,28 +1,28 @@
-//arquivo onde e configurado o servidor express
-
-//importar express
+// arquivo onde e configurado o servidor express
+// importar express
 import express from 'express';
 import routes from './routes';
 
 class App {
   constructor() {
-    //cria o server
+    // cria o server
     this.server = express();
-    /*chamas os metodos fora do constructor, caso contrario eles
-    nao serao executados*/
-    this.middlewares()
+    /* chamas os metodos fora do constructor, caso contrario eles
+    nao serao executados */
+    this.middlewares();
     this.routes();
   }
 
-  //metodo middlewares
-  middlewares(){
+  // metodo middlewares
+  middlewares() {
     this.server.use(express.json());
   }
-  //metodo routes chama o arquivo com as rotas
+
+  // metodo routes chama o arquivo com as rotas
   routes() {
     this.server.use(routes);
   }
 }
 
-//exporta o servidor para ser chamado em outros arquivos
+// exporta o servidor para ser chamado em outros arquivos
 export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,10 @@
+/* importa o metodo Router de dentro do express para nao importar o
+express inteiro */
+import { Router } from 'express';
 
-/*importa o metodo Router de dentro do express para nao importar o
-express inteiro*/
-import {Router} from 'express';
-
-//cria variavel routes que ira conter o metodo Routes
+// cria variavel routes que ira conter o metodo Routes
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-
-  return res.json({ message: 'Helo wadaorld'});
-
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
 export default routes;
